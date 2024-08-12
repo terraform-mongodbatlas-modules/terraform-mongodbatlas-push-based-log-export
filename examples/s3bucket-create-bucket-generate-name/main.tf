0b40c55b8f19aac1634825002d88c0181df66df2
rubenVB01 <rubenvibax@hotmail.com>
2024-08-12 12:40:09 +0200

    correct format files

--- examples/s3bucket-create-bucket-generate-name/main.tf
+++ examples/s3bucket-create-bucket-generate-name/main.tf
@@ -1,6 +1,6 @@
 module "s3" {
-  source        = "terraform-mongodbatlas-modules/push-based-log-export/mongodbatlas//modules/s3-bucket"
-  project_id    = var.project_id
+  source              = "terraform-mongodbatlas-modules/push-based-log-export/mongodbatlas//modules/s3-bucket"
+  project_id          = var.project_id
   use_existing_bucket = false
-  force_destroy = false
+  force_destroy       = false
 }
